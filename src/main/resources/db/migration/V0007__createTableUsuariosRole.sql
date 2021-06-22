0481ce10750552b2561f1592ff2cddabd93727fd
petry <thiagokeiserpetry@gmail.com>
2021-06-22 16:38:08 -0300

    Migration para a tabela dos usuários

---
+++ src/main/resources/db/migration/V0007__createTableUsuariosRole.sql
@@ -0,0 +1,10 @@
+CREATE TABLE role_usuarios (
+    usuarios_id bigint not null,
+    role_nome_role varchar(45) not null
+);
+
+ALTER TABLE role_usuarios ADD CONSTRAINT fk_usuarios
+FOREIGN KEY (usuarios_id) REFERENCES usuario (id);
+
+ALTER TABLE role_usuarios ADD CONSTRAINT fk_role
+FOREIGN KEY (role_nome_role) REFERENCES role (nome_role);
