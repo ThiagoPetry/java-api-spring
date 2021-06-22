a00d4cd84faa656e39a2aa8eb9d0f217917bd580
petry <thiagokeiserpetry@gmail.com>
2021-06-22 16:39:19 -0300

    Teste para criptografar a senha

--- src/main/java/br/com/senai/LogisticaApplication.java
+++ src/main/java/br/com/senai/LogisticaApplication.java
@@ -2,12 +2,14 @@ package br.com.senai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class LogisticaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LogisticaApplication.class, args);
+		//System.out.println(new BCryptPasswordEncoder().encode("123456"));
 	}
 
 }
